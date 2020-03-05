354370dfb6bb82e04eda628b98bb5924787e0f5f
Herrera <lucaas.pablo4@gmail.com>
2020-03-05 17:17:03 -0300

    Implementa HTML e CSS do Menu lateral
    Se for aplicado, este commit implementa a estrutura HTML e CSS do component menu

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [
         CommonModule,
+        RouterModule,
     ],
-    declarations: [ HeaderComponent ],
-    exports: [ HeaderComponent ],
+    declarations: [ 
+        HeaderComponent, 
+        NavbarComponent 
+    ],
+    exports: [ 
+        HeaderComponent,
+        NavbarComponent 
+    ],
 })
 
 export class CoreModule {}
